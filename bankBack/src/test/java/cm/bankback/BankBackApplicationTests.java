d4683acbcafe04bf9c244d6febd744671fdcdbda
unknown <bendevtech16@gmail.com>
2024-05-09 00:17:47 +0200

    backend bank init

---
+++ bankBack/src/test/java/cm/bankback/BankBackApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.bankback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
